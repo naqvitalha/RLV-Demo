334e009ab405dde05ea642fcf83d88565ec00eee
Talha Naqvi <naqvitalha@gmail.com>
2018-05-04 00:14:45 +0530

    Perf improvement

--- src/components/ImageRenderer.js
+++ src/components/ImageRenderer.js
@@ -1,38 +1,49 @@
-import React from 'react';
-import { Platform, View } from 'react-native';
-
-const isIOS = Platform.OS === 'ios';
+import React from "react";
+import { View } from "react-native";
 
 export class ImageRenderer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.id = 1;
+  }
   shouldComponentUpdate(newProps) {
     return this.props.imageUrl !== newProps.imageUrl;
   }
   componentWillUpdate() {
-    //On iOS while recycling till the new image is loaded the old one remains visible. This forcefully hides the old image.
-    //It is then made visible onLoad
-    //Will not be invoked on Android and Web
-    if (isIOS && this.imageRef) {
-      this.imageRef.setNativeProps({
-        opacity: 0,
-      });
+    if (this.imageRef) {
+      this.imageRef.style.opacity = 0;
     }
   }
-  handleOnLoad = () => {
-    if (isIOS && this.imageRef) {
-      this.imageRef.setNativeProps({
-        opacity: 1,
-      });
-    }
-  };
+  componentDidMount() {
+    let reqId = ++this.id;
+    requestIdleCallback(() => {
+      if (reqId === this.id && this.imageRef) {
+        this.imageRef.src = null;
+        this.imageRef.style.opacity = 1;
+        this.imageRef.src = this.props.imageUrl;
+      }
+    });
+  }
+  componentDidUpdate() {
+    let reqId = ++this.id;
+    requestIdleCallback(() => {
+      if (reqId === this.id && this.imageRef) {
+        this.imageRef.src = null;
+        this.imageRef.style.opacity = 1;
+        this.imageRef.src = this.props.imageUrl;
+      }
+    });
+  }
   render() {
     return (
       <View
         style={{
           flex: 1,
           margin: 3,
-          backgroundColor: 'lightgrey',
-        }}>
-          <img
+          backgroundColor: "lightgrey"
+        }}
+      >
+        <img
           ref={ref => {
             this.imageRef = ref;
           }}
@@ -41,8 +52,6 @@ export class ImageRenderer extends React.Component {
             width: "100%",
             objectFit: "cover"
           }}
-          onLoad={this.handleOnLoad}
-          src={this.props.imageUrl }
         />
       </View>
     );
