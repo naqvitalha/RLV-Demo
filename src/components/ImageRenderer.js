6c235386df0585432005d7915142ce6f9dcfef5e
Talha Naqvi <naqvitalha@gmail.com>
2018-07-01 10:15:57 +0530

    Update ImageRenderer.js

--- src/components/ImageRenderer.js
+++ src/components/ImageRenderer.js
@@ -1,35 +1,49 @@
-import React from "react";
-import { View } from "react-native";
+import React from 'react';
+import { Platform, View } from 'react-native';
+import { Image } from './ImageWeb.js';
+
+const isIOS = Platform.OS === 'ios';
 
 export class ImageRenderer extends React.Component {
   shouldComponentUpdate(newProps) {
     return this.props.imageUrl !== newProps.imageUrl;
   }
+  componentWillUpdate() {
+    //On iOS while recycling till the new image is loaded the old one remains visible. This forcefully hides the old image.
+    //It is then made visible onLoad
+    //Will not be invoked on Android and Web
+    if (isIOS && this.imageRef) {
+      this.imageRef.setNativeProps({
+        opacity: 0,
+      });
+    }
+  }
+  handleOnLoad = () => {
+    if (isIOS && this.imageRef) {
+      this.imageRef.setNativeProps({
+        opacity: 1,
+      });
+    }
+  };
   render() {
     return (
-     <View	
-        style={styles.container}	
-     >
-        <img
+      <View
+        style={{
+          flex: 1,
+          margin: 3,
+        }}>
+        <Image
           ref={ref => {
             this.imageRef = ref;
           }}
-          src={this.props.imageUrl}
-          style={styles.image}
+          style={{
+            flex: 1,
+          }}
+          onLoad={this.handleOnLoad}
+          defaultSource={{ uri: 'https://img1a.flixcart.com/www/linchpin/batman-returns/images/fk-default-image-75ff340b.png', scale: 0.2}}
+          source={{ uri: this.props.imageUrl }}
         />
       </View>
     );
   }
 }
-const styles = {
-  image: {
-     height: "100%",
-     width: "100%",
-     objectFit: "cover"
-  },
-  container: {
-     flex: 1,
-     margin: 3,
-     backgroundColor: "lightgrey"
-  }
-};
